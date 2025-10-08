a9ceff9d754be6cce559f5fac2672ac7dd75937f
Nes <neverliken@gmail.com>
2025-10-08 12:49:28 +0700

    InactiveStockCleaner

---
+++ InactiveStockCleaner/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InactiveStockCleaner
+{
+    public partial class InactiveStockCleaner_Form : Form
+    {
+        public InactiveStockCleaner_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
